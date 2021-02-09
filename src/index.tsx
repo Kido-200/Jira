28027f852b4ffb7e74dc849068ade03c41e3e6ac
unknown <1756894282@qq.com>
2021-02-09 15:53:18 +0800

    chore: 添加dev-tools

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { loadDevTools } from "jira-dev-tool";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+loadDevTools(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
+);
