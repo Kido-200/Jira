d8fad5493cc61e6051dd73a825ef66c969f5226a
unknown <1756894282@qq.com>
2021-02-09 10:58:20 +0800

    feat: 添加useMounce和useDebounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from 'react'
 import  SearchPanel  from "./search-panel"
 import List from './list'
-import { clearnObject } from 'utils'
+import { clearnObject, useDebounce, useMount } from 'utils'
 
 import * as qs from "qs"
 
@@ -14,6 +14,8 @@ const ProjectListScrren = () => {
     //select选择的负责人
     personId:''
   })
+  //比起传统的防抖保存函数,我们的effect因为是根据值改变而调用的,所以在hook里的防抖只需要保存dep值就好了
+  const paramDebounce = useDebounce(param,500)
   //select内容,所有项目负责人名称
   const [users,setUsers] = useState([])
 
@@ -21,20 +23,26 @@ const ProjectListScrren = () => {
 
   //返回input对应的项目  如果为空返回所有项目
   useEffect(()=>{
-    fetch(`${apiUrl}/projects?${qs.stringify(clearnObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(clearnObject(paramDebounce))}`).then(async response => {
       if(response.ok){
         setList(await response.json())
       }
     })
-  },[param])
+  },[paramDebounce])
 
-  useEffect(() => {
+
+
+
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if(response.ok){
         setUsers(await response.json())
       }
     })
-  },[])
+  })
+
+
+
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,6 @@
+import { useEffect, useState } from "react";
+import { callbackify } from "util";
+
 //单独处理0  null undefined会被 !value排除
 export const isFalsy = (value) => (value === 0 ? false : !value);
 //在一个函数里,改变传入的对象本身是不好的
@@ -11,4 +14,23 @@ export const clearnObject = (object) => {
     }
   });
   return result;
+};
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+//useDebounce作用是防抖保存value更新后的值
+//注意value需要是一个useState创建的state,这样他的改变才能触发重新渲染才能触发effect调用
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
 };
