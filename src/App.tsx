02b1b3c5684a8984caa31102c3fcca60692575ce
unknown <1756894282@qq.com>
2021-02-10 10:18:21 +0800

    feat: 添加登录注册系统

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { AuthenticatedApp } from "authenticated-app";
+import { useAuth } from "context/auth-context";
 import React from "react";
-import LoginScreen from "screens/login";
-import ProjectListScreen from "screens/project-list";
+
+import { UnauthenticatedApp } from "unauthenticated-app";
 
 function App() {
+  const { user } = useAuth();
+
   return (
     <div className="App">
-      {/* <ProjectListScreen /> */}
-      <LoginScreen />
+      {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
     </div>
   );
 }
--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -15,7 +15,6 @@ const LoginScreen = () => {
   };
   return (
     <form onSubmit={handleSubmit}>
-      {user ? <div> 登录成功,用户名:{user?.name} </div> : null}
       <div>
         <label htmlFor="username">用户名</label>
         <input type="text" id="username" />
